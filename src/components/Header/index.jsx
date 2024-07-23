71107a5a93b900389a6ae92946f534d4eb4f7bae
SkyNot <igorrian.cntgm18@gmail.com>
2024-07-22 22:18:02 -0300

    Implementando Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+return (
+    <header className={styles.header}>
+        <span className={styles.number}>(99) 9999-9999</span>
+    </header>
+)
+};
+export default Header
