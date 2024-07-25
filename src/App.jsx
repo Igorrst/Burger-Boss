5c892a140edd1e609a49f3ca82b85b84caca35ad
Kayky Dion <kayky.dion@gmail.com>
2024-07-25 19:01:32 -0300

    estilizando scrollbar do DrinkList

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import Header from './components/Header';
-import Cart from './containers/Cart/Cart';
-import Menu from './containers/Menu';
-import DrinkList from './containers/DrinkList';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Cart from "./containers/Cart/Cart";
+import Menu from "./containers/Menu";
+import DrinkList from "./containers/DrinkList";
+import Footer from "./components/Footer";
 
 const App = () => {
-    return (
-        <div>
-            <Header />
-            <div className='container'>
-            <Cart />
-            <Menu />
-            <DrinkList />
-            </div>
-            <Footer />
-            {}
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Cart />
+        <Menu />
+        <DrinkList />
+      </div>
+      <Footer />
+      {}
+    </div>
+  );
+};
 
 export default App;
